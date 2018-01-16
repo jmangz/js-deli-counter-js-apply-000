db2260d88b8356e3414036d6ee9cc453f08e5282
jmangz <echou1@binghamton.edu>
2018-01-16 22:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
   } else {
-    katzDeliLine.shift();
-    return katzDeliLine[0];
+    var serving = katzDeliLine.shift();
+    return `Currently serving ${serving}.`;
   }
 }
 

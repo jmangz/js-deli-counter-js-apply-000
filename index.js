77ea336d4e6224799a4409ad2d0fc3ada4a7ffc7
jmangz <echou1@binghamton.edu>
2018-01-16 22:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, name) {
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
+  return `Welcome, ${name}. You are number ${(katzDeliLine.indexOf(name) + 1)} in line.`;
 }
 
 function nowServing(katzDeliLine) {
